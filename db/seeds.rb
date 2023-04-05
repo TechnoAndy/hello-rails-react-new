ca977416142b7b24c315f3914cb77eec67fc33db
Andrea Tamara Diegaardt <atdiegaardt@gmail.com>
2023-04-05 21:38:09 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Message.create(greeting: "Hello, how are you?")
-Message.create(greeting: "Hi, how is your day going?")
-Message.create(greeting: "Hey, what's up?")
-Message.create(greeting: "Howdy")
-Message.create(greeting: "How you doin?")
+Greeting.create(greeting: "Hello, how are you?")
+Greeting.create(greeting: "Hi, how is your day going?")
+Greeting.create(greeting: "Hey, what's up?")
+Greeting.create(greeting: "Howdy")
+Greeting.create(greeting: "How you doin?")
 
